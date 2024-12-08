ff14411a8123062e7541c4d41ef59a51053be220
droidcrafts <contact@droidcrafts.com>
2024-12-08 08:04:12 +0545

    profile

---
+++ src/components/footer/FooterText.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  footerText: {
+    position: 'absolute',
+    bottom: theme.spacing(6),
+    left: theme.spacing(6),
+    '&:hover': {
+      color: theme.palette.primary.main,
+    },
+    transition: 'all 0.5s ease',
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+}));
